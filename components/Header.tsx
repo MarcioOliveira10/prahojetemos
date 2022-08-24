00360279eca4e9592a524e5be4d23dd50d5a1cf2
Marcio Souza <marciooliveira10@gmail.com>
2022-08-24 20:36:47 +0100

    feat: created Footer and Header

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+/** @jsxRuntime automatic */
+/** @jsxImportSource theme-ui */
+import styles from "styles/Home.module.css"
+import { Flex } from "theme-ui";
+
+function Header() {
+    return (
+        <Flex sx={{
+            bg: "primary",
+            width: "100%",
+            height: 200,
+
+        }}>
+
+        </Flex>
+    )
+}
+
+export default Header;
